95fe428c0397c0ea00fc7364149ee6e6a1693c05
Shiyan Chen <ch592127418@gmail.com>
2022-05-28 20:41:39 -0700

    login, login status, logout, signup APIs

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,47 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email') 
+        fields = ('username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
+class SignupSerializer(serializers.ModelSerializer):  # ModelSerializer表示serializer.save时能够把用户成功创建出来
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    # will be called when serializer.is_valid is called
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This username has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'message': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(  # 不能直接用create,create_user会把密码加密为密文
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user

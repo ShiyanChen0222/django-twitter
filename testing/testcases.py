c4bbb62e988d81e64455b6cdaa81cbb4d4c06c7a
Shiyan Chen <ch592127418@gmail.com>
2022-08-01 20:02:25 -0700

    switch friendships to hbase

--- testing/testcases.py
+++ testing/testcases.py
@@ -6,7 +6,7 @@ from tweets.models import Tweet
 from comments.models import Comment
 from likes.models import Like
 from newsfeeds.models import NewsFeed
-from friendships.models import Friendship
+from friendships.services import FriendshipService
 from rest_framework.test import APIClient
 from utils.redis_client import RedisClient
 from django_hbase.models import HBaseModel
@@ -55,7 +55,7 @@ class TestCase(DjangoTestCase):
         return User.objects.create_user(username, email, password)
 
     def create_friendship(self, from_user, to_user):
-        return Friendship.objects.create(from_user=from_user, to_user=to_user)
+        return FriendshipService.follow(from_user.id, to_user.id)
 
     def create_tweet(self, user, content=None):
         if content is None:
